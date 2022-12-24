10bf96086428fcbc0520a9eb8f8f5bb1af1a0da6
norbadboy <norbadboy@gmail.com>
2022-12-24 18:37:05 +0100

    fixed img homepage

--- src/js/api/listing/unregisteredList.mjs
+++ src/js/api/listing/unregisteredList.mjs
@@ -9,16 +9,17 @@ export async function renderList(list) {
     const itemContainer = document.createElement("div");
     itemContainer.classList.add("itemContainer");
     itemContainer.innerHTML = `
-                             <div class="card cardBody p-4">
+                             <div class="cardBody d-flex flex-column flex-grow-1 rounded text-center mt-3 p-4">
                                 <div class="itemTitle d-flex justify-content-center">
                                     <h1>${item.title.toUpperCase()}</h1>
                                 </div>
                                 <div class="itemImage mt-3">
                                      <img src="${item.media}" alt="${
       item.title
-    }" onerror="this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/320px-Image_not_available.png'">
+    }" class="img-thumbnail" onerror="this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/320px-Image_not_available.png'">
                                 </div>
-                                <div class="itemDescriptionContainer my-3 d-flex">
+                                <div class="itemInfoContainer"> 
+                                <div class="itemDescriptionBody my-3 d-flex">
                                 <p class="itemDescription">${item.description}</p>
                                 </div>
                                 <div class="itemBid my-3 d-flex justify-content-around">
@@ -36,6 +37,9 @@ export async function renderList(list) {
                                 </div>
                                 </div>
                             </div>
+                                </div>
+
+                                
             
     `;
     listContainer.appendChild(itemContainer);
